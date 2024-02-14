c59c472893720c673f3bcb2377ee84209618c061
SMM-LP-128 <SMM-LP-128@SMM-LP128>
2024-02-14 17:40:44 +0530

    Add project files.

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("GetStudentList")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string StudentName()
+        {
+            return "Student Name";
+            {
+
+            }
+        }
+    }
+}
